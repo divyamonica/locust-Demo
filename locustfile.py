66742472fdad4968300a04094b1713356fe63f48
Divya Monica <divyaanthony1512@gmail.com>
2025-07-14 22:49:23 +0530

    stress test

---
+++ locustfile.py
@@ -0,0 +1,16 @@
+from locust import HttpUser,task,between
+
+class MyStressTestUser(HttpUser):
+    wait_time = between(1,2)
+    
+    @task
+    def get_posts(self):
+        self.client.get('/')
+from locust import HttpUser,task,between
+
+#class MyStressTestUser(HttpUser):
+#  wait_time = between(1,2)
+    
+#   @task
+#   def get_posts(self):
+#        self.client.get('/posts')
