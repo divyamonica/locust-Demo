66c6a1a16ac6e7fea295842510488679e85f0f85
Divya Monica <divyaanthony1512@gmail.com>
2025-07-14 23:09:41 +0530

    post test

--- locustfile.py
+++ locustfile.py
@@ -5,12 +5,18 @@ class MyStressTestUser(HttpUser):
     
     @task
     def get_posts(self):
-        self.client.get('/')
-from locust import HttpUser,task,between
+        self.client.get('/posts')
+        
+    @task
+    def create_post(self):
+        self.client.post('/posts', json={"title": "New Post", "content": "This is a new post."})
+
+
+#from locust import HttpUser,task,between
 
 #class MyStressTestUser(HttpUser):
 #  wait_time = between(1,2)
     
 #   @task
 #   def get_posts(self):
-#        self.client.get('/posts')
+#        self.client.get('/')
